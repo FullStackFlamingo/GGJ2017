facf4c7a8142c7b1a01bfeebac87f03a8105e237
MathieuHalley <mathieu.halley@gmail.com>
2017-01-21 12:58:57 +0000

    EventManager

---
+++ Assets/Scripts/EventTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTest : MonoBehaviour {
+
+
+	public void OnEnable()
+	{
+		EventManager.StartListening(Events.WaveBeginEvent, OnWaveBegin);
+	}
+
+	public void OnDisable()
+	{
+		EventManager.StopListening(Events.WaveBeginEvent, OnWaveBegin);
+	}
+
+	public void Update()
+	{
+		EventManager.InvokeEvent(Events.WaveBeginEvent);
+	}
+
+	public void OnWaveBegin()
+	{
+		Debug.Log("WaveEvent");
+	}
+}
