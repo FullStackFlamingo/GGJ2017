a19a1a81280740990ca4252bcd499a116f9d7588
Georgina McDaniel <georgie.mcdaniel@gmail.com>
2017-01-21 17:46:08 +0000

    game scene, player collects item and throws

--- Assets/Scripts/Character/CharacterColliderController.cs
+++ Assets/Scripts/Character/CharacterColliderController.cs
@@ -5,18 +5,42 @@ using UnityEngine;
 public class CharacterColliderController : MonoBehaviour {
 
     public Collider triggerCollider;
+	public bool collidingWithObject = false; 
+	public GameObject collectedObject; 
+	private bool collectedObjectBool = false; 
      
+	private float speed = 5f;
 	// Use this for initialization
 	void Start () {
-		
+		collectedObjectBool = false; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (collidingWithObject == true && Input.GetKeyDown(KeyCode.Space)) {
+			collectedObject.transform.SetParent(GameObject.FindGameObjectWithTag("Player").transform);
+			collectedObjectBool = true; 
+	
+		}
+		if (collectedObjectBool == true && Input.GetKeyDown (KeyCode.Q)) {
+			collectedObject.transform.SetParent (null);
+			Rigidbody rb = collectedObject.GetComponent<Rigidbody> (); 
+			rb.isKinematic = false;  
+			rb.velocity = new Vector3(20, 10, 0);
+			collectedObjectBool = false; 
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
-        Debug.Log(other);
-    }
+		if (other.gameObject.tag != "boat") {
+		 	//if (collectedObjectBool = false) { 
+				Debug.Log ("CLKSHFKJSBNVDKSEJVBKJ");
+				collidingWithObject = true; 
+				collectedObject = other.gameObject; 
+				Rigidbody rb = collectedObject.GetComponent<Rigidbody> (); 
+	 
+				rb.mass = 1;
+		 //}
+		}
+	}
 }
