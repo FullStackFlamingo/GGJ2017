cc27465a7b4735079b438ccfae47ee655e7f91c6
GeorgieMcD <georgie.mcdaniel@gmail.com>
2017-01-21 14:49:02 +0000

    added in boat and barriers and camera

---
+++ Assets/Scripts/GameScene/GameSceneController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Game scene controller, spawns and keeps track of items on ship and invokes the wave methods.
+
+public class GameSceneController : MonoBehaviour {
+
+	public GameObject player; 
+	public GameObject boat; 
+	public GameObject[] itemPositions;
+	public GameObject[] items;  
+
+	void Start () {
+ 
+		itemPositions = GameObject.FindGameObjectsWithTag("itemPosition");
+		items = GameObject.FindGameObjectsWithTag("item");
+		InvokeRepeating("InvokeWave", 1.0f, 5f);
+	}
+	
+ 
+	void Update () {
+ 
+	}
+
+	public void InvokeWave() {
+
+		//randomly istanciate an item in a 
+		int numberOfObjectsToInstanciate = 10; 
+
+		for (int i = 0; i < numberOfObjectsToInstanciate; i++) {
+
+			 GameObject newObj = Instantiate (items[Random.Range(0,items.Length)], itemPositions [Random.Range (0, itemPositions.Length)].transform.position,  Quaternion.identity);
+			newObj.transform.SetParent(GameObject.FindGameObjectWithTag("boat").transform);
+		}
+		 
+	 
+	} 
+
+
+
+
+
+
+}
