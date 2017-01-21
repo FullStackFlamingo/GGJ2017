f2ae0d5a6e8f0ef7f1b931fb0e2b32a74703d683
Richard <contactajaxlexer@gmail.com>
2017-01-21 16:19:09 +0000

    tween library - working on ui

---
+++ Assets/Scripts/ui/Progress.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Progress : MonoBehaviour {
+
+	public Image boatImage;
+
+	private GameObject boatImageObject;
+	// Use this for initialization
+	void Start () {
+		boatImageObject = boatImage.gameObject;
+		var rotationProperty = new RotationTweenProperty( new Vector3( 0, 0, 10 ) );
+
+		var config = new GoTweenConfig();
+		config.addTweenProperty( rotationProperty );
+
+		config.setIterations(-1,GoLoopType.PingPong);
+		var tween = new GoTween( boatImageObject.transform, 0.6f, config );
+		
+		Go.addTween( tween );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
