486930e32ef52d655b4169b71a91ee35dbb6b5b7
Richard <contactajaxlexer@gmail.com>
2017-01-21 14:34:19 +0000

    rocking world

---
+++ Assets/Scripts/BoatRock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatRock : MonoBehaviour {
+
+	private Quaternion rotation;
+	private float currentAngle = 0;
+	// Use this for initialization
+	void Start () {
+		rotation = this.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentAngle +=0.03f;
+		//Debug.Log(Mathf.Sin(currentAngle));
+		//Debug.Log(Mathf.Sin(currentAngle)*45);
+		rotation.SetEulerAngles(0,0,Mathf.Sin(currentAngle)*10*Mathf.Deg2Rad);
+		this.transform.rotation = rotation;
+	}
+}
--- Assets/Scripts/Character/CharacterMovementController.cs
+++ Assets/Scripts/Character/CharacterMovementController.cs
@@ -5,18 +5,38 @@
  {
      public float speed = 2f;
      public float turnSmoothing = 15f;
+
+     //public LayerMask layerMask;
      private Vector3 movement;
      private Rigidbody playerRigidBody;
      
      private Quaternion targetRotation;
 
+     private Vector3 tmpV1;
+     private Vector3 tmpV2;
+
+     private float targetYPos = 0;
+
+     //RaycastHit hit; 
+     
      void Awake()
      {
          playerRigidBody = GetComponent<Rigidbody> ();
+         tmpV1 = new Vector3();
+         tmpV2 = new Vector3();
+         //layerMask = ~layerMask;
      }
      
      void FixedUpdate()
      {
+         /*tmpV1 = transform.position;
+         tmpV1.y=10;
+
+        if (Physics.Raycast(tmpV1, -Vector3.up, out hit, 50,layerMask)){
+            targetYPos = hit.point.y;
+        }*/ ///RAYCASTING DISABLED
+
+
          float lh = Input.GetAxisRaw ("Horizontal");
          float lv = Input.GetAxisRaw ("Vertical");
     
@@ -30,12 +50,13 @@
          //direction by camera
          movement = Camera.main.transform.TransformDirection(movement);
          
-         
          movement = movement.normalized * speed * Time.deltaTime;
         
-
-         
-         playerRigidBody.MovePosition (transform.position + movement);
+         tmpV1 = transform.position + movement;
+         //tmpV1.y = targetYPos; ///RAYCASTING DISABLED
+         //Debug.Log(targetYPos);
+         //Debug.Log(tmpV1.y);
+         playerRigidBody.MovePosition(tmpV1);
          
          
          if (lh != 0f || lv != 0f) 
