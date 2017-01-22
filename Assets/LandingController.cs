4539baf80d2bf6e8e38841137a181db81adf43e6
Richard <contactajaxlexer@gmail.com>
2017-01-22 12:38:23 +0000

    no message

--- Assets/LandingController.cs
+++ Assets/LandingController.cs
@@ -3,40 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class LandingController : MonoBehaviour {
 
 	public Image logo;
 	public Image howToPlay;
+
+	private bool keyPressReady = false;
 	// Use this for initialization
 	void Start () {
 		scaleInImage(logo);
 	}
-	
-		void scaleInImage(Image _image){
-			var scaleIng = new ScaleTweenProperty( new Vector3( 1, 1, 1 ) );
 
-			var config = new GoTweenConfig();
-			config.addTweenProperty( scaleIng );
-			config.setEaseType(GoEaseType.ElasticOut);
-			config.delay = 0.5f;
-						config.onComplete((AbstractGoTween abs)=>{
-				Debug.Log("dfdsdf");
-				scaleInImage(howToPlay);
-			});
-			var tween = new GoTween( _image.transform, 2f, config );
+	void scaleInImage(Image _image){
+		var scaleIng = new ScaleTweenProperty( new Vector3( 1, 1, 1 ) );
 
-			Go.addTween( tween );
-		}
+		var config = new GoTweenConfig();
+		config.addTweenProperty( scaleIng );
+		config.setEaseType(GoEaseType.ElasticOut);
+		config.delay = 0.5f;
+		config.onComplete((AbstractGoTween abs)=>{
+			scaleInImage(howToPlay);
+			keyPressReady = true;
+		});
+		var tween = new GoTween( _image.transform, 2f, config );
+
+		Go.addTween( tween );
+	}
 
 
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.anyKey && keyPressReady){
+					SceneManager.LoadScene("NewGameScene");
+		}
 	}
 
+	
+
 	public void onClickToStart(){
-		SceneManager.LoadScene("NewGameScene");
 
 	}
 	
