2b0fdddbeedac482fdf3f6c6a3c180d728c24418
Richard <contactajaxlexer@gmail.com>
2017-01-22 15:24:34 +0000

    no message

--- Assets/Scripts/GameUIController.cs
+++ Assets/Scripts/GameUIController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameUIController : MonoBehaviour {
-	private bool gameOver = false;
 	private bool gameEnd = false;
 	// Use this for initialization
 
@@ -19,25 +18,27 @@ public class GameUIController : MonoBehaviour {
 
 	public void showLoseScreen(){
 		loseScreen.SetActive(true);
+			StartCoroutine(enableRestart());
 	}
 	public void showWinScreen(){
 		winScreen.SetActive(true);
 	}
 
+
     IEnumerator enableRestart() {
-        yield return new WaitForSeconds(2);
-        gameOver = true;
+        yield return new WaitForSeconds(4);
+		gameEnd = true;
     }
     
 	// Update is called once per frame
 	void Update () {
-		if(Input.anyKey && gameOver){
+		if(Input.anyKey && gameEnd){
 			SceneManager.LoadScene("NewGameScene");
 		}
 		time += Time.deltaTime;
 		currentTime = time;
 		if(time>maxTimeSeconds && !gameEnd){
-			gameEnd = true;
+			StartCoroutine(enableRestart());
 			showWinScreen();
 		}
 	}
