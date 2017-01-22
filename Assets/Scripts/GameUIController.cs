2ca48a11e7d48d0b074ed0a88500e6f24d25fca2
Richard <contactajaxlexer@gmail.com>
2017-01-22 13:46:56 +0000

    no message

---
+++ Assets/Scripts/GameUIController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameUIController : MonoBehaviour {
+	private bool gameOver = false;
+	// Use this for initialization
+
+	public GameObject winScreen;
+	public GameObject loseScreen;
+	void Start () {
+		
+	}
+
+	public void showLoseScreen(){
+		loseScreen.SetActive(false);
+	}
+	public void showWinScreen(){
+		winScreen.SetActive(true);
+	}
+
+    IEnumerator enableRestart() {
+        yield return new WaitForSeconds(2);
+        gameOver = true;
+    }
+    
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKey && gameOver){
+			SceneManager.LoadScene("NewGameScene");
+		}
+	}
+}
